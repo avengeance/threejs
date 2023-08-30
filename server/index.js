b9c8ff355aed3b96fd24d5002e6f29fb6a689ddd
Calvin Le <email.calvinle@gmail.com>
2023-08-29 20:41:54 -0700

    moved from dall-e route to index

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { Configuration, OpenAIApi } from "openai";
 
 import dalleRoutes from "./routes/dalle.routes.js";
 
@@ -10,10 +11,34 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
 app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({
+      photo: image,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 app.listen(8080, () => console.log("Server has started on port 8080"));
